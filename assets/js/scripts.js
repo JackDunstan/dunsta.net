933a1ebb2caa54b925a9b70e498c8999cc893aa8
Jack Dunstan <jack.dunstan@rmit.edu.au>
2022-01-08 21:10:08 +1100

    Addition of site

---
+++ assets/js/scripts.js
@@ -0,0 +1,14 @@
+//makes background gradient follow cursor
+$(document).mousemove(function(event) {
+    windowWidth = $(window).width();
+    windowHeight = $(window).height();
+
+    mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+    mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+
+    $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #2875a8, #714185)');
+});
+//tooltip
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
